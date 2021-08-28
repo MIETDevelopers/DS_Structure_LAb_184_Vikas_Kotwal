14fd2dd36747be4ed2c44493e2a0e172208322f2
vikas kotwal <76261604+vikaskotwal2002@users.noreply.github.com>
2021-08-28 09:27:23 +0530

    Add files via upload

---
+++ Postfix.c
@@ -0,0 +1,65 @@
+#include<stdio.h> 
+#include<ctype.h>	
+int stack[20];  
+int top = -1;  
+int n1,n2,n3,num;
+
+void push(int x)
+{
+    top=top+1;
+    stack[top]=x;
+}
+	
+int pop()
+{
+   return stack[top--]; 
+}
+
+	int main()
+	{
+	    char exp[20];
+	    char *e;	;
+	    printf("Enter the expression: ");
+	    scanf("%s",exp);
+	    e = exp;
+	    while(*e)             
+	    {
+	        if(isdigit(*e))
+	        {
+	            num = *e -'0'; 
+	            push(num);
+	        }
+	        else
+	        {
+	            n1 = pop();
+	            n2 = pop();
+	            switch(*e)
+	            {
+	            case '+':
+	            {
+	                n3 = n1 + n2;
+	                break;
+	            }
+	            case '-':
+	            {
+	                n3 = n2 - n1;
+	                break;
+	            }
+	            case '*':
+	            {
+	                n3 = n1 * n2;
+	                break;
+	            }
+	            case '/':
+	            {
+	                n3 = n2 / n1;
+	                break;
+	            }
+	            }
+	            push(n3);
+	        }
+	        e++;
+	    }
+	    printf("\nResult is: %d",pop());
+return 0;	
+}
